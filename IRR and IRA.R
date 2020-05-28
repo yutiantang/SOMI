1ed112f36888aa130aea78988a2b63405f407e28
Yutian Thompson <42153791+yutiantang@users.noreply.github.com>
2020-05-28 16:23:47 -0500

    update somi

--- IRR and IRA.R
+++ IRR and IRA.R
@@ -5,6 +5,8 @@
 library(multilevel)
 library(psych)
 library(dplyr)
+library(irr)
+library(ICC)
 
 
 #data----------------------------
@@ -21,7 +23,9 @@ df<-data.frame(
   
 )
 
-data("lq2002")
+data("lq2002", package = "multilevel") #for awg 
+
+data("diagnoses", package = "irr") #for kappa
 
 sf <- matrix(c(9,    2,   5,    8,
                6,    1,   3,    2,
@@ -57,6 +61,10 @@ des<- as.data.frame(psych::describe(lq2002))
 
  #calculate icc by package
  psych::ICC(sf)
+irr::icc(sf, model = "oneway", unit = "average")
+irr::icc(sf, model = "twoway", type="agreement", unit = "average")
+irr::icc(sf, model = "twoway", type="consistency", unit = "average")
+
  
  
  
@@ -92,9 +100,63 @@ des<- as.data.frame(psych::describe(lq2002))
      d_sum = sum(d)
    )
  
-
- #kappa
+ diagnoses2<-diagnoses %>% 
+   dplyr::mutate(
+     rater1 = recode(rater1, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+     rater2 = recode(rater2, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+     rater3 = recode(rater3, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+     rater4 = recode(rater4, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+     rater5 = recode(rater5, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+     rater6 = recode(rater6, 
+                     `1. Depression`           = 1L,
+                     `2. Personality Disorder` = 2L,
+                     `3. Schizophrenia`        = 3L,
+                     `4. Neurosis`             = 4L,
+                     `5. Other`                = 5L),
+   )
  
+ 
+ 
+ #kappa
+ head(diagnoses[, 1:3])
+ #Cohen's kappa
+ irr::kappa2(diagnoses[, c("rater1", "rater2")], weight = "unweighted")
+ #weighted kappa
+ irr::kappa2(diagnoses[, c("rater1", "rater2")], weight = "equal")
+ #Fleiss kappa
+ irr::kappam.fleiss(diagnoses[, 1:3])
+ #Kendall
+ irr::kendall(diagnoses, TRUE)
+ 
+ #--save_data--------
+ #save the example data to run in spss
+ write.csv(diagnoses, file = "C:/Users/YTHOMPSO/Documents/GitHub/SOMI/SOMI_May2020/diagnoses.csv", row.names = F)
+ write.csv(diagnoses2, file = "C:/Users/YTHOMPSO/Documents/GitHub/SOMI/SOMI_May2020/diagnoses2.csv", row.names = F)
  
  
  
