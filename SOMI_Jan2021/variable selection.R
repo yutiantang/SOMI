7d25346eea80e64e4d02ed6e2310cf1ce8953506
Yutian Thompson <42153791+yutiantang@users.noreply.github.com>
2021-01-12 15:20:40 -0600

    Update variable selection.R

--- SOMI_Jan2021/variable selection.R
+++ SOMI_Jan2021/variable selection.R
@@ -2,7 +2,7 @@
 #Topic: Variable Selection
 #Edit Date: 12/23/2021
 #Author: Yutian T. Thompson
-#note: This repo is for the SOMI lecture 
+#note: This repo is for the SOMI lecture Jan 2021
 
 rm(list=ls(all=TRUE))
 # ---- load-sources -----------------------------------------------------------------
@@ -19,7 +19,11 @@ library(glmnet)
 
 
 #---load-data-----------------
+#please specify your data reading path. 
+#Data is from http://www.tqmp.org/RegularArticles/vol13-1/p001/
 student <- read.table(file = "C:/Users/YTHOMPSO/Dropbox/Work/Teaching/2021SOMI_1/student-mat.csv", sep=";",header = T)
 
+
+#---tweak-data----------------
 
 
