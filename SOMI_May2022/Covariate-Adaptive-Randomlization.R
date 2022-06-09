ea9894966fc4091be8e4ae14f6c18d34f440749f
Yutian Thompson <42153791+yutiantang@users.noreply.github.com>
2022-06-09 16:24:34 -0500

    Update Covariate-Adaptive-Randomlization.R

--- SOMI_May2022/Covariate-Adaptive-Randomlization.R
+++ SOMI_May2022/Covariate-Adaptive-Randomlization.R
@@ -1,7 +1,7 @@
 #SOMI May 2022
 #Author: Yutian T. Thompson
-#Topic: covariate adaptive randomization
-#Purpose:
+#Topic: An introduction of covariate adaptive randomization
+
 
 
 library(randomizr) #another randomization package
@@ -58,7 +58,7 @@ ds_sample %>%
   dplyr::tibble()
 
 
-#A simple randomisation
+#A simple randomization --> #unrestricted randomization
 set.seed(12345)
 index <- round(length(x)*0.5,digits=1)
 test.indices <- sample(1:length(x), index)
@@ -66,7 +66,148 @@ sr<-randomizr::complete_ra(20, prob = 0.5)
 sum(sr)
 
 
-#practice Pocock-Simon Minimization
+
+#practice permuted block randomization
+pbr <- c(1L, 2L, 3L, 4L, 5L, 6L)
+sample(pbr, 6, replace  = F)
+
+
+#historical fun
+coin_study <- c("00011101001111101000110101111000100111001000001110
+                00101010100100001001100010000111010100010000101101
+                01110100001101001010000011111011111001101100101011
+                01010000011000111001111101101010110100110110110110
+                01111100001110110001010010000010100111111011101011
+                10001100011000110001100110100100001000011101111000
+                11111110000000001101011010011111011110010010101100
+                11101101110010000010001100101100111110100111100010
+                00001001101011101010110011111011001000001101011111
+                11010001111110010111111001110011111111010000100000
+                00001111100101010111100001110111001000110100001111
+                11000101001111111101101110110111011010010110110011
+                01010011011111110010111000111101111111000001001001
+                01001110111011011011111100000101010101010101001001
+                11101101110011100000001001101010011001000100001100
+                10111100010011010110110111001101001010100000010000
+                00001011001101011011111000101100101000011100110011
+                11100101011010000110001001100010010001100100001001
+                01000011100000011101101111001110011010101101001011
+                01000001110110100010001110010011100001010000000010
+                10010001011000010010100011111101101111010101010000
+                01100010100000100000000010000001100100011011101010
+                11011000110111010110010010111000101101101010110110
+                00001011011101010101000011100111000110100111011101
+                10001101110000010011110001110100001010000111110100
+                00111111111111010101001001100010111100101010001111
+                11000110101010011010010111110000111011110110011001
+                11111010000011101010111101101011100001000101101001
+                10011010000101111101111010110011011110000010110010
+                00110110101111101011100101001101100100011000011000
+                01010011000110100111010000011001100011101011100001
+                11010111011110101101101111001111011100011011010000
+                01011110100111011001001110001111011000011110011111
+                01101011101110011011100011001111001011101010010010
+                10100011010111011000111110000011000000010011101011
+                10001011101000101111110111000001111111011000000010
+                10111111011100010000110000110001111101001110110000
+                00001111011100011101010001011000110111010001110111
+                10000010000110100000101000010101000101100010111100
+                00101110010111010010110010110100011000001110000111")
+
+
+jek <- as.numeric(coin_study)
+
+jek <- as.numeric(strsplit(as.character(coin_study), "")[[1]]) %>%
+  as.data.frame() %>%
+  dplyr::rename( result = ".") %>%
+  dplyr::filter(!is.na(result)) %>%
+  dplyr::mutate(
+    times = 1:dplyr::n()
+  )
+
+jek %>%
+  dplyr::filter(times <= 120) %>% #you can change the number here to check about his experiemntal results
+  dplyr::summarise(
+    total = sum(result, na.rm=T)
+  )
+
+# ---- stratified-block --------------------------------------------------------
+ds_sample_strata <- ds_sample %>%
+  dplyr::mutate(
+    strata = dplyr::case_when(region_final=="Midwestern" & screen_tool_final==TRUE~"strata1",
+                               region_final=="Midwestern" & screen_tool_final==FALSE~"strata2",
+                              region_final=="Northeast" & screen_tool_final==TRUE~"strata3",
+                              region_final=="Northeast" & screen_tool_final==FALSE~"strata4",
+                              region_final=="Southern" & screen_tool_final==TRUE~"strata5",
+                              region_final=="Southern" & screen_tool_final==FALSE~"strata6",
+                              region_final=="Western" & screen_tool_final==TRUE~"strata7",
+                              region_final=="Western" & screen_tool_final==FALSE~"strata8"),
+  ) %>%
+  dplyr::arrange(strata) %>%
+  dplyr::mutate(
+    id= 1:dplyr::n()
+  )
+
+# ds_sample_strata_use <- ds_sample_strata %>%
+#   dplyr::select(-UniqueID)
+
+pIDs <- new("PatientID",
+            strata = c("strata1", "strata2", "strata3", "strata4", "strata5", "strata6", "strata7", "strata8"),
+            start  = c(1L, 15L, 20L, 33L, 37L, 64L, 68L, 77L), #the start and stop point drawn from the data new created "id"
+            stop   = c(14L, 19L, 32L, 36L, 63L, 67L, 76L, 81L))
+
+validPID(pIDs)
+
+trts = c( A = 1L, B = 1L, C = 1L)
+pbdesc = new("PermutedBlockDesc", treatments = trts, type="PermutedBlock",
+             numBlocks=27L)
+CE1 <- new("ClinicalExperiment",
+           name       = "Navigator Randomization",
+           treatments = trts,
+           factors    = list(F1 = c("A", "B", "C"), F2 = c("TRUE", "FALSE"),
+                             F3 = c("Midwestern", "Northeast", "Southern", "Western")),
+           strataFun = function(pDesc) pDesc@strata,
+           randomization = list(strata1= list(pbdesc), strata2=list(pbdesc), strata3=list(pbdesc), strata4=list(pbdesc),
+                                strata5=list(pbdesc), strata6=list(pbdesc), strata7=list(pbdesc), strata8=list(pbdesc)),
+           patientIDs = pIDs)
+
+CT1 <- createTrial(CE1, seed = c(120, 378, 456, 225, 610, 783, 832, 706)) #this is a random seed, set up any values you wish
+
+#table(dempb<- randPack:::.newPBlock(pbdesc))
+
+
+make_LD = function(data,i){
+  new("PatientData", patientID = data$UniqueID[i], date=Sys.Date(),
+      covariates=list(screen_tool_final = data$screen_tool_final[i], region_final=data$region_final[i]), strata=data$strata[i])
+} #since this function is not generalized enough, I leave it here rather than save into the navigator_function repo
+
+
+ds_sample_use <- ds_sample_strata %>%
+  dplyr::mutate(UniqueID          = as.character(UniqueID),
+                screen_tool_final = as.character(screen_tool_final),
+                region_final      = as.character(region_final))
+
+pd_list <- list()
+for (i in 1:81){
+  pd_list[[i]]   <- assign(paste0("pd", i, sep=""), make_LD(ds_sample_use, i))
+}
+
+
+tyt_list <- list()
+for (j in 1:81) {
+  tyt_list[j] <- getTreatment(CT1, pd_list[[j]])
+
+}
+
+assignment <- do.call(rbind, tyt_list) %>% as.data.frame() %>%
+  dplyr::mutate(id = 1:dplyr::n()) %>%
+  dplyr::left_join(ds_sample_use, by="id")  %>%
+  dplyr::select(-screen_tool_final, -region_final)
+
+
+
+
+#---- Pocock-Simon Minimization-------------------
 
 trt1 <- res
 #Display the number of randomized subjects at covariate factors
